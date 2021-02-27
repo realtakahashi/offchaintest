7bc13b06a87042c115a14596d38c1965b9b384f5
shin.takahashi <realtakahashi.0924@gmail.com>
2021-02-27 21:29:02 +0900

    add http fetch

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,27 @@
 /// Learn more about FRAME and the core library of Substrate FRAME pallets:
 /// https://substrate.dev/docs/en/knowledgebase/runtime/frame
 
-use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, traits::Get};
+use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, traits::Get, debug};
 use frame_system::ensure_signed;
+use sp_runtime::{
+	offchain::http,
+	transaction_validity::{
+	  TransactionValidity, TransactionLongevity, ValidTransaction, InvalidTransaction
+	}
+};
+use sp_std::vec::Vec;
+//use parity_scale_codec::{Decode, Encode};
 
 #[cfg(test)]
 mod mock;
 
 #[cfg(test)]
 mod tests;
+
+
+//use alt_serde::{Deserialize, Deserializer};
+
+const HTTP_HEADER_USER_AGENT: &str = "realtakahashi";
 
 /// Configure the pallet by specifying the parameters and types on which it depends.
 pub trait Trait: frame_system::Trait {
@@ -63,6 +76,12 @@ decl_module! {
 		// Events must be initialized if they are used by the pallet.
 		fn deposit_event() = default;
 
+		fn offchain_worker(block: T::BlockNumber) {
+			match Self::fetch_data() {
+			  Ok(res) => debug::info!("Result: {}", core::str::from_utf8(&res).unwrap()),
+			  Err(e) => debug::error!("Error fetch_data: {}", e),
+			};
+		  }
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		#[weight = 10_000 + T::DbWeight::get().writes(1)]
@@ -101,3 +120,66 @@ decl_module! {
 		}
 	}
 }
+
+// #[serde(crate = "alt_serde")]
+// #[derive(Deserialize, Encode, Decode, Default)]
+// struct GithubInfo {
+// 	// Specify our own deserializing function to convert JSON string to vector of bytes
+// 	#[serde(deserialize_with = "de_string_to_bytes")]
+// 	login: Vec<u8>,
+// 	#[serde(deserialize_with = "de_string_to_bytes")]
+// 	body: Vec<u8>,
+// 	update_at: DateTime<Utc>,
+// 	created_at: DateTime<Utc>,
+// }
+
+// impl fmt::Debug for GithubInfo {
+// 	// `fmt` converts the vector of bytes inside the struct back to string for
+// 	//   more friendly display.
+// 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+// 		write!(
+// 			f,
+// 			"{{ login: {}, body: {}, created_at: {}, update_at: {} }}",
+// 			str::from_utf8(&self.login).map_err(|_| fmt::Error)?,
+// 			str::from_utf8(&self.body).map_err(|_| fmt::Error)?,
+// 			&self.created_at,
+// 			&self.update_at,
+// 		)
+// 	}
+// }
+
+impl<T: Trait> Module<T> {
+	fn fetch_data() -> Result<Vec<u8>, &'static str> {
+  
+	  // Specifying the request
+//	  let pending = http::Request::get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD")
+	let pending = http::Request::get("https://api.github.com/repos/realtakahashi/faucet_pallet/issues/2/comments")
+		.add_header("User-Agent", HTTP_HEADER_USER_AGENT)
+		.send()
+		.map_err(|_| "Error in sending http GET request")?;
+  
+	  // Waiting for the response
+	  let response = pending.wait()
+		.map_err(|_| "Error in waiting http response back")?;
+  
+	  // Check if the HTTP response is okay
+	  if response.code != 200 {
+		debug::warn!("Unexpected status code: {}", response.code);
+		return Err("Non-200 status code returned from http request");
+	  }
+ 
+	//   let resp_bytes = response.body().collect::<Vec<u8>>();
+
+	//   let resp_str = str::from_utf8(&resp_bytes).map_err(|_| <Error<T>>::HttpFetchingError)?;
+	//   // Print out our fetched JSON string
+	//   debug::info!("{}", resp_str);
+
+	//   // Deserializing JSON to struct, thanks to `serde` and `serde_derive`
+	//   let gh_info: GithubInfo =
+	// 	  serde_json::from_str(&resp_str).map_err(|_| <Error<T>>::HttpFetchingError)?;
+
+	//   debug::info!("{}", gh_info);
+	  // Collect the result in the form of bytes
+	  Ok(response.body().collect::<Vec<u8>>())
+	}
+  }
