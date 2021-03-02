224fdb7559c33e2f18389a7feee7c3dd7c573d4e
shin.takahashi <realtakahashi.0924@gmail.com>
2021-03-02 20:51:03 +0900

    pass serde_json

--- src/lib.rs
+++ src/lib.rs
@@ -4,25 +4,44 @@
 /// Learn more about FRAME and the core library of Substrate FRAME pallets:
 /// https://substrate.dev/docs/en/knowledgebase/runtime/frame
 
-use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, traits::Get, debug};
-use frame_system::ensure_signed;
-use sp_runtime::{
-	offchain::http,
-	transaction_validity::{
-	  TransactionValidity, TransactionLongevity, ValidTransaction, InvalidTransaction
-	}
-};
-use sp_std::vec::Vec;
-//use parity_scale_codec::{Decode, Encode};
-
 #[cfg(test)]
 mod mock;
 
 #[cfg(test)]
 mod tests;
 
+use core::{convert::TryInto, fmt};
+use frame_support::{
+	debug, decl_error, decl_event, decl_module, decl_storage, dispatch::DispatchResult,dispatch,
+};
+use parity_scale_codec::{Decode, Encode};
 
-//use alt_serde::{Deserialize, Deserializer};
+use frame_system::{
+	self as system, ensure_none, ensure_signed,
+	offchain::{
+		AppCrypto, CreateSignedTransaction, SendSignedTransaction, SendUnsignedTransaction,
+		SignedPayload, SigningTypes, Signer, SubmitTransaction,
+	},
+};
+use sp_core::crypto::KeyTypeId;
+use sp_runtime::{
+	RuntimeDebug,
+	offchain as rt_offchain,
+	offchain::{
+		http,
+		storage::StorageValueRef,
+		storage_lock::{StorageLock, BlockAndTime},
+	},
+	transaction_validity::{
+		InvalidTransaction, TransactionSource, TransactionValidity,
+		ValidTransaction,
+	},
+};
+use sp_std::{
+	prelude::*, str,
+	collections::vec_deque::VecDeque,
+};
+use alt_serde::{Deserialize, Deserializer};
 
 const HTTP_HEADER_USER_AGENT: &str = "realtakahashi";
 
@@ -62,6 +81,8 @@ decl_error! {
 		NoneValue,
 		/// Errors should have helpful documentation associated with them.
 		StorageOverflow,
+		// Error returned when fetching github info
+		HttpFetchingError,
 	}
 }
 
@@ -84,7 +105,7 @@ decl_module! {
 		  }
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[weight = 10_000 + T::DbWeight::get().writes(1)]
+		#[weight = 10_000]
 		pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
 			// Check that the extrinsic was signed and get the signer.
 			// This function will return an error if the extrinsic is not signed.
@@ -101,7 +122,7 @@ decl_module! {
 		}
 
 		/// An example dispatchable that may throw a custom error.
-		#[weight = 10_000 + T::DbWeight::get().reads_writes(1,1)]
+		#[weight = 10_000]
 		pub fn cause_error(origin) -> dispatch::DispatchResult {
 			let _who = ensure_signed(origin)?;
 
@@ -121,40 +142,107 @@ decl_module! {
 	}
 }
 
-// #[serde(crate = "alt_serde")]
-// #[derive(Deserialize, Encode, Decode, Default)]
+#[serde(crate = "alt_serde")]
+#[derive(Deserialize, Encode, Decode, Default)]
+struct User{
+	#[serde(deserialize_with = "de_string_to_bytes")]
+	login: Vec<u8>,
+	id: u32,
+	#[serde(deserialize_with = "de_string_to_bytes")]
+	node_id: Vec<u8>,
+	#[serde(deserialize_with = "de_string_to_bytes")]
+	avatar_url: Vec<u8>,
+	#[serde(deserialize_with = "de_string_to_bytes")]
+	gravatar_id: Vec<u8>,
+	#[serde(deserialize_with = "de_string_to_bytes")]
+	url: Vec<u8>,
+	#[serde(deserialize_with = "de_string_to_bytes")]
+	html_url: Vec<u8>,
+	#[serde(deserialize_with = "de_string_to_bytes")]
+	followers_url: Vec<u8>,
+	#[serde(deserialize_with = "de_string_to_bytes")]
+	following_url: Vec<u8>,
+	#[serde(deserialize_with = "de_string_to_bytes")]
+	gists_url: Vec<u8>,
+	#[serde(deserialize_with = "de_string_to_bytes")]
+	starred_url: Vec<u8>,
+	#[serde(deserialize_with = "de_string_to_bytes")]
+	subscriptions_url: Vec<u8>,
+	#[serde(deserialize_with = "de_string_to_bytes")]
+	organizations_url: Vec<u8>,
+	#[serde(deserialize_with = "de_string_to_bytes")]
+	repos_url: Vec<u8>,
+	#[serde(deserialize_with = "de_string_to_bytes")]
+	events_url: Vec<u8>,
+	#[serde(deserialize_with = "de_string_to_bytes")]
+	received_events_url: Vec<u8>,
+	#[serde(deserialize_with = "de_string_to_bytes")]
+	r#type: Vec<u8>,
+	site_admin: bool,
+}
+
+#[serde(crate = "alt_serde")]
+#[derive(Deserialize, Encode, Decode, Default)]
+struct GithubInfo {
+	//Specify our own deserializing function to convert JSON string to vector of bytes
+	#[serde(deserialize_with = "de_string_to_bytes")]
+	url: Vec<u8>,
+	#[serde(deserialize_with = "de_string_to_bytes")]
+	node_id: Vec<u8>,
+	user: User,
+	#[serde(deserialize_with = "de_string_to_bytes")]
+	created_at: Vec<u8>,
+	#[serde(deserialize_with = "de_string_to_bytes")]
+	updated_at: Vec<u8>,
+	#[serde(deserialize_with = "de_string_to_bytes")]
+	body: Vec<u8>,
+}
+
 // struct GithubInfo {
 // 	// Specify our own deserializing function to convert JSON string to vector of bytes
 // 	#[serde(deserialize_with = "de_string_to_bytes")]
 // 	login: Vec<u8>,
 // 	#[serde(deserialize_with = "de_string_to_bytes")]
-// 	body: Vec<u8>,
-// 	update_at: DateTime<Utc>,
-// 	created_at: DateTime<Utc>,
+// 	blog: Vec<u8>,
+// 	public_repos: u32,
 // }
 
-// impl fmt::Debug for GithubInfo {
-// 	// `fmt` converts the vector of bytes inside the struct back to string for
-// 	//   more friendly display.
-// 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-// 		write!(
-// 			f,
-// 			"{{ login: {}, body: {}, created_at: {}, update_at: {} }}",
-// 			str::from_utf8(&self.login).map_err(|_| fmt::Error)?,
-// 			str::from_utf8(&self.body).map_err(|_| fmt::Error)?,
-// 			&self.created_at,
-// 			&self.update_at,
-// 		)
-// 	}
-// }
+pub fn de_string_to_bytes<'de, D>(de: D) -> Result<Vec<u8>, D::Error>
+where
+	D: Deserializer<'de>,
+{
+	let s: &str = Deserialize::deserialize(de)?;
+	Ok(s.as_bytes().to_vec())
+}
+
+impl fmt::Debug for GithubInfo {
+	// `fmt` converts the vector of bytes inside the struct back to string for
+	//   more friendly display.
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		write!(
+			f,
+			"{{ url: {}, node_id: {}, login: {}, created_at: {}, updated_at: {}, body: {} }}",
+			// "{{ url: {}, node_id: {}, login: {}, created_at: {}, updated_at: {}, body: {} }}",
+			str::from_utf8(&self.url).map_err(|_| fmt::Error)?,
+			str::from_utf8(&self.node_id).map_err(|_| fmt::Error)?,
+			str::from_utf8(&self.user.login).map_err(|_| fmt::Error)?,
+			str::from_utf8(&self.created_at).map_err(|_| fmt::Error)?,
+			str::from_utf8(&self.updated_at).map_err(|_| fmt::Error)?,
+			str::from_utf8(&self.body).map_err(|_| fmt::Error)?,
+		)
+	}
+}
+
+// pub const HTTP_REMOTE_REQUEST: &str = "https://api.github.com/orgs/substrate-developer-hub";
+pub const HTTP_REMOTE_REQUEST: &str = "https://api.github.com/repos/realtakahashi/faucet_pallet/issues/2/comments";
 
 impl<T: Trait> Module<T> {
 	fn fetch_data() -> Result<Vec<u8>, &'static str> {
   
-	  // Specifying the request
-//	  let pending = http::Request::get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD")
-	let pending = http::Request::get("https://api.github.com/repos/realtakahashi/faucet_pallet/issues/2/comments")
+	// Specifying the request
+	let pending = http::Request::get(HTTP_REMOTE_REQUEST)
 		.add_header("User-Agent", HTTP_HEADER_USER_AGENT)
+		.add_header("Accept-Charset", "UTF-8")
 		.send()
 		.map_err(|_| "Error in sending http GET request")?;
   
@@ -168,17 +256,23 @@ impl<T: Trait> Module<T> {
 		return Err("Non-200 status code returned from http request");
 	  }
  
-	//   let resp_bytes = response.body().collect::<Vec<u8>>();
+	  let resp_bytes = response.body().collect::<Vec<u8>>();
 
-	//   let resp_str = str::from_utf8(&resp_bytes).map_err(|_| <Error<T>>::HttpFetchingError)?;
-	//   // Print out our fetched JSON string
-	//   debug::info!("{}", resp_str);
+	  let resp_str = str::from_utf8(&resp_bytes).map_err(|_| <Error<T>>::HttpFetchingError)?;
+	  // Print out our fetched JSON string
+	  debug::info!("$$$$ response string $$$$");
+	  debug::info!("{}", resp_str);
 
-	//   // Deserializing JSON to struct, thanks to `serde` and `serde_derive`
-	//   let gh_info: GithubInfo =
-	// 	  serde_json::from_str(&resp_str).map_err(|_| <Error<T>>::HttpFetchingError)?;
+	  let resp_str2 = resp_str.replace(r"\r\n","<br>");
+	  debug::info!("$$$$ response string22222 $$$$");
+	  debug::info!("{}", resp_str2);
 
-	//   debug::info!("{}", gh_info);
+	  // Deserializing JSON to struct, thanks to `serde` and `serde_derive`
+	  let gh_info: Vec<GithubInfo> =
+		  serde_json::from_str(&resp_str2).map_err(|_| <Error<T>>::HttpFetchingError)?;
+
+	  debug::info!("$$$$ json string $$$$");
+	  debug::info!("{:#?}", gh_info);
 	  // Collect the result in the form of bytes
 	  Ok(response.body().collect::<Vec<u8>>())
 	}
